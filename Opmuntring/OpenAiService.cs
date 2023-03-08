aa2a3345db2f1bfa57b2c077cbc4213a888b753e
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2023-03-08 17:09:06 +0100

    Add project files.

---
+++ Opmuntring/OpenAiService.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace Opmuntring
+{
+    public class OpenAiService
+    {
+
+        private static HttpWebRequest CreateRequest(string url)
+        {
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.Method = "GET";
+
+            return request;
+        }
+
+        private static string GetResponseAsString(WebResponse response)
+        {
+            var responseStream = response.GetResponseStream();
+            var reader = new StreamReader(responseStream);
+            string html = reader.ReadToEnd();
+            return html;
+        }
+
+
+        public static async Task<string> promptGpt3Async(string prompt, int limit, string token)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+            var request = new HttpRequestMessage();
+
+            request.Method = HttpMethod.Post;
+            request.RequestUri = new Uri("https://api.openai.com/v1/completions");
+
+            var json = JsonConvert.SerializeObject(new
+            {
+                model = "text-davinci-003",
+                max_tokens = limit,
+                temperature = 0.7,
+                top_p = 1,
+                prompt = prompt,
+                frequency_penalty = 0,
+                presence_penalty = 00
+            });
+
+
+            request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.SendAsync(request);
+            var responseText = await response.Content.ReadAsStringAsync();
+            dynamic obj = JsonConvert.DeserializeObject(responseText);
+            string reason = obj.choices[0].finish_reason;
+            if (reason == "length")
+            {
+                return await promptGpt3Async(prompt, limit * 2, token);
+            }
+            string text = obj.choices[0].text;
+            return text;
+        }
+    }
+}
---
+++ Opmuntring/Pages/Index.cshtml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Opmuntring.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+        private static string[] grader = {"Let", "Udfordrende", "Meget svært" };
+       
+
+        public List<String> Errors { get; set; }
+
+        public string Quote { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public async Task OnGetAsync(String navn, String fag, String tidspunkt, String udfordring)
+        {
+            int skala = 0;
+
+            Errors = new List<String>();
+            if (String.IsNullOrEmpty(navn))
+            {
+                Errors.Add("Jeg havde regnet med at få indsendt et felt, der hedder navn, men det fik jeg ikke");
+            }
+
+            if (String.IsNullOrEmpty(fag))
+            {
+                Errors.Add("Jeg havde regnet med at få indsendt et felt, der hedder fag, men det fik jeg ikke");
+            } else
+            {
+                if (fag != "Webudvikling" && fag != "Cloud" && fag != "Webprogrammering frontend")
+                {
+                    Errors.Add($"Jeg kender kun fagene Webudvikling, Cloud, og Webprogrammering frontend. Du indsendte {fag}");
+                }
+            }
+
+            if (String.IsNullOrEmpty(tidspunkt))
+            {
+                Errors.Add("Jeg havde regnet med at få indsendt et felt, der hedder tidspunkt, men det fik jeg ikke");
+            }
+            else
+            {
+                if (tidspunkt != "morgen" && fag != "eftermiddag" && fag != "aften")
+                {
+                    Errors.Add($"Jeg kender kun tidspunkterne morgen, eftermiddag og aften. Du indsendte {tidspunkt}");
+                }
+            }
+
+            if (String.IsNullOrEmpty(udfordring))
+            {
+                Errors.Add("Jeg havde regnet med at få indsendt et felt, der hedder udfordring, men det fik jeg ikke");
+            } else
+            {
+                if (!int.TryParse(udfordring, out skala))
+                {
+                    Errors.Add($"Jeg havde regnet med, at udfordring var et heltal, men det er det ikke. Det er {udfordring}");
+                } else if (skala < 1 || skala > 3)
+                {
+                    Errors.Add($"Jeg havde regnet med, at udfordring var et tal mellem 1 og 3 (inkl), men det er det ikke. Det er {udfordring}");
+                } else
+                {
+                    skala = skala - 1; // Indsendes fra 1-3 men skal bruges i array
+                }
+            }
+
+            if (Errors.Count > 0)
+            {
+                return;
+            }
+
+            string token = _config["Gpt3Token"];
+            String prompt = $"Fortælhvorfor det er fantastisk for {navn} at lære om {fag} om {tidspunkt}en, selvom det er {grader[skala]}";
+            Quote = await OpenAiService.promptGpt3Async(prompt, 200, token);
+            
+        }
+    }
+}
